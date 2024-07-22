98612a21b98d6eedf84a2076aaafd30e4b51986f
Jamyle Elen <Jamyle Elen>
2024-07-22 20:56:17 -0300

    feat: new route (getItemByCategory)

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,11 @@
 
 import express from 'express'
-import { getAllItems, getItemById, createItem, updateItem, deleteItem } from '../controllers/itemController.js'
+import { getAllItems, getItemById, getItemByCategory, createItem, updateItem, deleteItem } from '../controllers/itemController.js'
 
 const router = express.Router();
 
 router.get('/items', getAllItems)
+router.get('/items/category', getItemByCategory)
 router.get('/items/:id', getItemById);
 router.post('/items', createItem);
 router.put('/items:id', updateItem)
