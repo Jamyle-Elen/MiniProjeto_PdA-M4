2f13ebed26a201190520cba01f2e033b0cbf5911
Jamyle Elen <Jamyle Elen>
2024-07-21 22:09:57 -0300

    feat: database connection file

---
+++ src/config/database.js
@@ -0,0 +1,24 @@
+
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    portt: process.env.PORTT || 3306
+
+})
+
+connection.connect(err => {
+    if (err) {
+        console.log('Error error connecting to database', err);
+        return
+    }
+    console.log('Conected to database, OK!')
+})
+
+export default connection;
