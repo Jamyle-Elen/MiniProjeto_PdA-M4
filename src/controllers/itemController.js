be55df8c06bf4ea85b882a853dd7dc5e882f25f0
Jamyle Elen <Jamyle Elen>
2024-07-21 22:16:21 -0300

    feat: connection to SQL, item creation, insertion into the database, features (getAllItems, getItemById, updateItem and deleteItem)

---
+++ src/controllers/itemController.js
@@ -0,0 +1,97 @@
+
+import connection from "../config/database.js";
+import Item from "../models/Items.model.js";
+
+// criaçoa do item
+export const createItem = (req, res) => {
+  const { id, name, price, manufacturing_date, expiry_date, category, quantity } =
+    req.body;
+
+    const newItem = new Item(
+        name,
+        price,
+        manufacturing_date,
+        expiry_date,
+        category,
+        quantity);
+    const sql = "INSERT INTO items SET ?";
+// conexao com o sql
+  connection.query(sql, newItem, (err, results) => {
+    if (err) {
+      console.log("Erro ao inserir o item", err)
+      return res.status(500).json({ error: "Erro ao inserir o item" });
+    }
+    res.status(201).json({ message: 'Item adicionado com sucesso', item: newItem });
+    if (!isValidDate(manufacturing_date) || !isValidDate(expiry_date)) {
+        return res.status(400).json({ error: 'Formato de data inválido' });
+      }
+  })
+}
+
+export const getAllItems = (req, res) => {
+    const sql = 'SELECT * FROM items';
+
+    connection.query(sql, (err, results) => {
+        if (err) {
+            console.error('Erro ao inserir item', err);
+            return res.status(500).json({ error: 'Erro ao inserir item' })
+        }
+        res.status(200).json(results);
+    })
+}
+
+// pegar item usando o ID
+
+export const getItemById = (req, res) => {
+    const { id } = req.params
+    // diferença de params e body
+    const sql = 'SELECT * FROM items WHERE id = ?'
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) {
+            console.error('Erro ao pegar item', err);
+            return res.status(500).json({ error: 'Erro ao pegar item' })
+        }
+        if (results.length === 0) {
+            return res.status(404).json({ error: 'Item não encontrado' })
+        }
+        res.status(200).json(results[0]);
+    })
+}
+
+// atualizar item (put) então vai ser atualizar o item por completo
+export const updateItem = (req, res) => {
+    const { id } = req.params;
+    const { name, price, manufacturing_date, expiry_date, category, quantity } = req.body;
+
+    const sql = 'UPDATE items SET name = ?, price = ?, manufacturing_date = ?, expiry_date = ?, category = ?, quantity = ? '
+
+    connection.query(sql, [name, price, manufacturing_date, expiry_date, category, quantity, id], (err, results) => {
+        if (err) {
+            console.error('Erro ao atualizar item', err);
+            return res.status(500).json({ error: 'Erro ao atualizar item' })
+        }
+        // affectedRows é utilizado pra mostrar quantas linhas foram alteradas
+        if (results.affectedRows === 0) {
+            console.error('Item não encontradop')
+        }
+        res.status(200).json({ message: 'Item atualizado!' })
+    })
+}
+
+// deeletar item
+export const deleteItem = (req, res) => {
+    const { id } = req.params;
+    const sql = 'DELETE FROM items WHERE id = ?'
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) {
+            console.error('Erro ao deletar item', err)
+            return res.status(500).json({ error: 'Erro ao deletar item' })
+        }
+        if (results.affectedRows === 0) {
+            console.error('Item não encontrado')
+        }
+        res.status(200).json({ mensage: 'Item deletado com sucesso' })
+    })
+}
