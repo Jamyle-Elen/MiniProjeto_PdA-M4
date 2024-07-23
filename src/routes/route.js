1e82a34d8241ee1e47b099d9bd4a732d6481a06a
Jamyle Elen <Jamyle Elen>
2024-07-23 01:53:33 -0300

    feat: added route for promotional items (getPromotionalItems) + import organization

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,13 @@
 
 import express from 'express'
-import { getAllItems, getItemById, getItemByCategory, createItem, updateItem, deleteItem } from '../controllers/itemController.js'
+import { getAllItems, getItemByCategory, getItemById, getPromotionalItems,createItem, updateItem, deleteItem } from '../controllers/itemController.js'
 
 const router = express.Router();
 
 router.get('/items', getAllItems)
 router.get('/items/category', getItemByCategory)
 router.get('/items/:id', getItemById);
+router.get('/items/promotional', getPromotionalItems);
 router.post('/items', createItem);
 router.put('/items:id', updateItem)
 router.delete('/items:id', deleteItem);
